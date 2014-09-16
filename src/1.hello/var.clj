78606af467a68eedc16a1c24ba17399275c23e22
Lellansin <lellansin@gmail.com>
2014-09-16 13:51:06 +0800

    update

--- src/1.hello/var.clj
+++ src/1.hello/var.clj
@@ -1,9 +1,11 @@
 ; literal
 (println (type 1 ))                            ; java.lang.Long
-(println (type 1N ))                           ; clojure.lang.BigInt
 (println (type 1.0))                           ; java.lang.Double
 (println (type \a))                            ; java.lang.Character
 (println (type "a"))                           ; java.lang.String
+(println (type true))                          ; java.lang.Boolean
+(println (type 1N ))                           ; clojure.lang.BigInt
+(println (type :cat))                          ; clojure.lang.Keyword
 (println (type [1,2,3,4]))                     ; clojure.lang.PersistentVector
 (println (type {:key "name" :value "Alan"}))   ; clojure.lang.PersistentArrayMap
 
@@ -13,3 +15,8 @@
 (println (type (byte 0)))                      ; java.lang.Byte
 (println (type (float 1.23)))                  ; java.lang.Float
 (println (type (double 1.23)))                 ; java.lang.double
+(println (type (boolean false)))               ; java.lang.Boolean
+(println (type (class 'str)))                  ; java.lang.Class
+(println (type (str "hello")))                 ; java.lang.String
+(println (type (list 1 2 3)))                  ; clojure.lang.PersistentList
+(println (type (vector 1 2 3)))                ; clojure.lang.PersistentVector
