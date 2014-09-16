38442c17e613b18d53f59878a0ec0f8ea9b31275
Lellansin <lellansin@gmail.com>
2014-09-16 13:57:01 +0800

    update computer

--- src/1.hello/compute.clj
+++ src/1.hello/compute.clj
@@ -1,5 +1,18 @@
-(println (+ 7 3)) ; 10
-(println (- 8 5)) ; 3
-(println (* 4 3)) ; 12
-(println (/ 3 2)) ; 3/2
-(println (inc 9)) ; 10
+(println "-------- arithmetic --------")
+(println (+ 7 3))  ; 10
+(println (- 8 5))  ; 3
+(println (* 4 3))  ; 12
+(println (/ 3 2))  ; 3/2
+(println (= 9 9))  ; true
+(println (inc 9))  ; 10
+(println (dec 9))  ; 8
+
+(println "---------- logic -----------")
+(println (and true false true)) ; false
+(println (and true true true))  ; true
+(println (and 1 2))             ; 2
+(println (and 1 2 3))           ; 3
+(println (or false 2 3))        ; 2
+(println (or false nil))        ; nil
+(println (not 2))               ; false
+(println (not nil))             ; true
